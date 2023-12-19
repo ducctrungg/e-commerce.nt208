459e570d59558b8dd3f6f69abc8f50da383257af
hai2036 <21522036@gm.uit.edu.vn>
2023-12-19 21:22:12 +0700

    add CRUD module for admin (missing CRUD for customer/user)

--- backend/mysite/main/models.py
+++ backend/mysite/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 
@@ -14,9 +15,10 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    description = models.CharField(max_length=1000, null=True)
+    image = models.ImageField(default="", null=True, blank=True)
     type = models.CharField(max_length=200, null=True)
-    slug = models.SlugField(default="", null=False)
+    slug = models.SlugField(default="", null=False, unique=True)
     def __str__(self):
         return self.name
     
@@ -33,7 +35,7 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True,null=True)
     address = models.CharField(max_length=200, null=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
-    date_ordered = models.DateTimeField(auto_now_add=True)
+    date_ordered = models.DateTimeField(default=datetime.datetime.now())
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
@@ -59,8 +61,11 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True,null=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True,null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(default=datetime.datetime.now())
 
+    def __str__(self):
+        return str(self.id)
+    
     @property
     def get_total(self):
         total = 0
